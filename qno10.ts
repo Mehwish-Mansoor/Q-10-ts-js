533f763b5c285bf73cf39dcd9e95d1c308cce826
Mehwish-Mansoor <160300292+Mehwish-Mansoor@users.noreply.github.com>
2024-03-26 05:12:41 +0500

    Add files via upload

---
+++ qno10.ts
@@ -0,0 +1,9 @@
+//  10. Adding comments: Choose two of the programs you've written, and add atleast one comment to each. 
+//  if you don't have any thing specific to write because your programs are too simple at this point' just add your name and the 
+//  current date at the top of each program File. Then write one sentence describing what the program does.
+
+let a: number = 40
+let b: number = 30
+
+console.log(`${a}+${b}=${a+b}`);
+console.log(`${a}*${b}=${a*b}`);
